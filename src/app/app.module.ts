d46b14b7d5a26788a0fc3e0062f4c55ce30e34a8
RashmiChandran <rashmichandran3@gmail.com>
2020-06-06 00:06:55 +0530

    Updated list view UI and added add-task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListViewComponent } from './components/list-view/list-view.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatChipsModule } from '@angular/material/chips';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListViewComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatSlideToggleModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
